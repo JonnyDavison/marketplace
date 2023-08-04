2125b9c9a7836d301062fff597327fd24ee0bce6
JonnyDavison <jonnydavison@icloud.com>
2023-08-04 03:15:20 +0000

    Add STATIC_ROOT to settings.py

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    
+
     'market',
 ]
 
@@ -145,6 +145,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
