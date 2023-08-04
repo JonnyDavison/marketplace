c2bc30a865498573ccaf3fde6fa88afa5bb11293
JonnyDavison <jonnydavison@icloud.com>
2023-08-04 03:05:02 +0000

    Install allauth and add to settings, urls and update requirements

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -42,6 +42,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    
     'market',
 ]
 
@@ -73,6 +79,15 @@ TEMPLATES = [
         },
     },
 ]
+AUTHENTICATION_BACKENDS = [ 
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend', 
+]
+
+SITE_ID = 1
 
 WSGI_APPLICATION = 'marketplace.wsgi.application'
 
--- marketplace/urls.py
+++ marketplace/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('accounts/', include('allauth.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
