e9aa5d291d595514ea992c9d33cff48da4105c05
JonnyDavison <jonnydavison@icloud.com>
2023-08-04 12:08:54 +0000

    Set up base & index templates, add views & urls

--- market/views.py
+++ market/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    """ a view to retuern the index page """
+    return render(request, 'market/index.html')
