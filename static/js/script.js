48e5245ca95d1bc5778a09a317b7ae3065846605
JonnyDavison <jonnydavison@icloud.com>
2023-08-05 10:32:05 +0000

    Clear unnesscessary code in script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,13 @@
 // Variables
-
 // Getting the stars
 let one = document.getElementById('first')
 let two = document.getElementById('second')
 let three = document.getElementById('third')
 let four = document.getElementById('fourth')
 let five = document.getElementById('fifth')
+// Gets the ratings form
+let starForm = document.querySelector('.ratings')
+
 
 // Stars array loops as cursour passes over the stars
 let stars = [one, two, three, four, five]
@@ -18,57 +20,29 @@ stars.forEach(item=> item.addEventListener('mouseover', (event)=>{
 let starSelect = (selection) => {
     switch(selection){
         case 'first': {
-            // one.classList.add('checked')
-            // two.classList.remove('checked')
-            // three.classList.remove('checked')
-            // four.classList.remove('checked')
-            // five.classList.remove('checked')
             addCheckedClass(1)
             return
         }
         case 'second': {
-            // one.classList.add('checked')
-            // two.classList.add('checked')
-            // three.classList.remove('checked')
-            // four.classList.remove('checked')
-            // five.classList.remove('checked')
             addCheckedClass(2)
             return
         }
         case 'third': {
-            // one.classList.add('checked')
-            // two.classList.add('checked')
-            // three.classList.add('checked')
-            // four.classList.remove('checked')
-            // five.classList.remove('checked')
             addCheckedClass(3)
             return
         }
         case 'fourth': {
-            // one.classList.add('checked')
-            // two.classList.add('checked')
-            // three.classList.add('checked')
-            // four.classList.add('checked')
-            // five.classList.remove('checked')
             addCheckedClass(4)
             return
         }
         case 'fifth': {
-            // one.classList.add('checked')
-            // two.classList.add('checked')
-            // three.classList.add('checked')
-            // four.classList.add('checked')
-            // five.classList.add('checked')
             addCheckedClass(5)
             return
         }
     }
 }
 
-let starForm = document.querySelector('.ratings')
-let csrf = document.getElementsByName('csrfmiddlewaretoken')
-
-
+// Adds/removes the checked class from the star button
 let addCheckedClass = (size) =>{
     let fromChildren = starForm.children
     for (let i=0; i < fromChildren.length; i++){
