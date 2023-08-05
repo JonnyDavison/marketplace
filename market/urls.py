52728a595c20f3c55db619f44515e7a3f15768a1
JonnyDavison <jonnydavison@icloud.com>
2023-08-05 09:11:11 +0000

    Add stars in the html to use for rating

--- market/urls.py
+++ market/urls.py
@@ -5,5 +5,6 @@ from market.views import rating_view
 urlpatterns = [
     path('', views.index, name='index'),
     path('market_posts/', views.PostList.as_view(), name='market_posts'),
-    path('market/<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('market/<slug:slug>/', views.PostDetail.as_view(), 
+         name='post_detail'),
 ]
