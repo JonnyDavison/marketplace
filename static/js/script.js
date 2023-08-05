ffd20253bda22a68686a81e7f2efb1bb5e274ee5
JonnyDavison <jonnydavison@icloud.com>
2023-08-05 09:32:15 +0000

    Add array and test loop

--- static/js/script.js
+++ static/js/script.js
@@ -7,4 +7,8 @@ let three = document.getElementById('third')
 let four = document.getElementById('fourth')
 let five = document.getElementById('fifth')
 
-console.log(one)
+let stars = [one, two, three, four, five]
+
+stars.forEach(item=> item.addEventListener('mouseover', (event)=>{
+    console.log(event.target)
+}))
