e6c595b6ac4c4807eafa465e66b076bc1f9f5118
JonnyDavison <jonnydavison@icloud.com>
2023-08-05 09:26:14 +0000

    Add ids to star spans and add to JS and test

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,10 @@
-console.log('hello world')
+// Variables
+
+// Getting the stars
+let one = document.getElementById('first')
+let two = document.getElementById('second')
+let three = document.getElementById('third')
+let four = document.getElementById('fourth')
+let five = document.getElementById('fifth')
+
+console.log(one)
