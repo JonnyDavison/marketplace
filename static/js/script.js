2be0302e21d75ca2e1574a177e60eab2263cdc47
JonnyDavison <jonnydavison@icloud.com>
2023-08-05 12:56:58 +0000

    Add getNumericValue to asign a value for ratings

--- static/js/script.js
+++ static/js/script.js
@@ -7,17 +7,48 @@ let four = document.getElementById('fourth')
 let five = document.getElementById('fifth')
 // Gets the ratings form
 let starForm = document.querySelector('.ratings')
+// Stars Array
+let stars = [one, two, three, four, five]
+
+
 
 // Stars array loops as cursour passes over the stars
-let stars = [one, two, three, four, five]
 stars.forEach(item=> item.addEventListener('mouseover', (event)=>{
     starSelect(event.target.id)
 }))
 
+// Get numeric value
+let getNumericValue = (stringValue) => {
+    let numericValue;
+    if(stringValue == 'first'){
+        numericValue = 1
+    } else if (stringValue == 'second') {
+        numericValue = 2
+    } else if (stringValue == 'third') {
+        numericValue = 3
+    } else if (stringValue == 'fourth') {
+        numericValue = 4
+    } else if (stringValue == 'fifth') {
+        numericValue = 5
+    } else {
+        numericValue = 0
+    }
+    return numericValue
+}
+
 stars.forEach(item=> item.addEventListener('click', (event)=>{
     let starValue = event.target.id
-    alert(starValue)
+    starRating = getNumericValue(starValue)
+    console.log(starRating)
+
+    starForm.addEventListener('submit', e=>{
+        e.preventDefault()
+        let starId = e.target.id
+    //     console.log(starId)
+    })
 }))
+
+
 
 
 // Select stars and add the class 'checked'
