0c1d1a2d1f526ca7b349f092f82d03f399a57e41
JonnyDavison <jonnydavison@icloud.com>
2023-08-09 16:17:26 +0000

    Add forms and admin for rating

--- market/forms.py
+++ market/forms.py
@@ -1,4 +1,4 @@
-from .models import Comment
+from .models import Comment, ReviewRating
 from django import forms
 
 
@@ -6,3 +6,8 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = ReviewRating
+        fields = ['subject', 'review', 'rating']
--- market/urls.py
+++ market/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path, include
 from . import views
-from market.views import rating_view, rateing_value
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('market_posts/', views.PostList.as_view(), name='market_posts'),
     path('market/<slug:slug>/', views.PostDetail.as_view(),
          name='post_detail'),
-    path('market/rate/', rateing_value, name='rateing_value'),
+    path('market/submit_review/<int:post_id>', views.submit_review, name='submit_review'),
 ]
