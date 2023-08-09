869b107ce91f1568cc53c7dc52ea9adc0c77810e
JonnyDavison <jonnydavison@icloud.com>
2023-08-09 16:22:28 +0000

    Update urls.py for submitting a review

--- market/urls.py
+++ market/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('market_posts/', views.PostList.as_view(), name='market_posts'),
     path('market/<slug:slug>/', views.PostDetail.as_view(),
          name='post_detail'),
-    path('market/submit_review/<int:post_id>', views.submit_review, name='submit_review'),
+    path('market/submit_review/<int:post_id>', views.submit_review,
+         name='submit_review'),
 ]
