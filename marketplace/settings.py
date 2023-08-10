fd2f94692a8a20ddaadaadb27bf600a984637330
JonnyDavison <jonnydavison@icloud.com>
2023-08-10 15:09:30 +0000

    Add messages in settings and display in base.html

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-
+from django.contrib.messages import constants as messages
 if os.path.isfile('env.py'):
     import env
 
@@ -29,6 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['8000-jonnydaviso-marketplace-kxhz8xrxb60.ws-eu102.gitpod.io']
 # ALLOWED_HOSTS = ['https://market-8c7ce1538eb0.herokuapp.com/']
@@ -94,6 +96,15 @@ AUTHENTICATION_BACKENDS = [
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 WSGI_APPLICATION = 'marketplace.wsgi.application'
