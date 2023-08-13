e7379e4dafc2ef525ffe3a64b1dc7987f9c11dea
JonnyDavison <jonnydavison@icloud.com>
2023-08-13 08:46:34 +0000

    Create allowed users and admin only decorators

--- market/decorators.py
+++ market/decorators.py
@@ -16,7 +16,28 @@ def allowed_users(allowed_roles=[]):
     def decorator(view_func):
         def wrapper_func(request, *args, **kwargs):
 
-            print('Working:', allowed_roles)
-            return view_func(request, *args, **kwargs)
+            group = None
+            if request.user.groups.exists():
+                group = request.user.groups.all()[0].name
+
+            if group in allowed_roles:
+                return view_func(request, *args, **kwargs)
+            else:
+                return HttpResponse('Your dont have the credntials for this page')
         return wrapper_func
     return decorator
+
+
+def admin_only(view_func):
+    def wrapper_function(request, *args, **kwargs):
+        group = None
+        if request.user.groups.exists():
+            group = request.user.groups.all()[0].name
+
+        if group == 'customer':
+            return redirect('user-page')
+
+        if group == 'admin':
+            return view_func(request, *args, **kwargs)
+
+    return wrapper_function
