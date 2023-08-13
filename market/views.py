b7934fccffc5b2599427713d8ddfdc6c424f6992
JonnyDavison <jonnydavison@icloud.com>
2023-08-13 07:56:23 +0000

    Create decorators.py

--- market/views.py
+++ market/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .models import *
 from .forms import orderForm, CreateUserForm
+from .decorators import unauthenticated_user
 
 
 @login_required(login_url='login')
@@ -108,40 +109,35 @@ def deleteOrder(request, pk):
     return render(request, 'market/delete.html', context)
 
 
+@unauthenticated_user
 def registerPage(request):
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
 
-    if request.user.is_authenticated:
-        return redirect('index')
-    else:
-        form = CreateUserForm()
-        if request.method == 'POST':
-            form = CreateUserForm(request.POST)
-            if form.is_valid():
-                form.save()
-                user = form.cleaned_data.get('username')
-                messages.success(request, 'Account was created for ' + user)
+            return redirect('login')
 
-                return redirect('login')
-
-        context = {'form': form}
+    context = {'form': form}
     return render(request, 'market/register.html', context)
 
 
+@unauthenticated_user
 def loginPage(request):
-    if request.user.is_authenticated:
-        return redirect('index')
-    else:
-        if request.method == 'POST':
-            username = request.POST.get('username')
-            password = request.POST.get('password')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
 
-            user = authenticate(request, username=username, password=password)
+        user = authenticate(request, username=username, password=password)
 
-            if user is not None:
-                login(request, user)
-                return redirect('index')
-            else:
-                messages.info(request, 'Username OR password is incorrect')
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.info(request, 'Username OR password is incorrect')
 
         context = {}
         return render(request, 'market/login.html', context)
