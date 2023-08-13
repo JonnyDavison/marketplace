773435765f8bb169c99f3cae14185224e0554a85
JonnyDavison <jonnydavison@icloud.com>
2023-08-13 06:34:51 +0000

    Create the login and logout views

--- market/views.py
+++ market/views.py
@@ -3,6 +3,8 @@ from django.views import generic, View
 from django.http import HttpResponseRedirect
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .models import *
 from .forms import orderForm, CreateUserForm
@@ -118,7 +120,25 @@ def registerPage(request):
 
 
 def loginPage(request):
-    context = {
+    if request.user.is_authenticated:
+        return redirect('index')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
 
-    }
-    return render(request, 'market/login.html', context)
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+            else:
+                messages.info(request, 'Username OR password is incorrect')
+
+        context = {}
+        return render(request, 'market/login.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
