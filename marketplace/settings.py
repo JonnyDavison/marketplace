acf59ee1aea699e5ec7af4ce6692434bbbe00125
JonnyDavison <jonnydavison@icloud.com>
2023-08-13 01:14:59 +0000

    Update, base, index, status.html, add basic css

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -34,7 +34,7 @@ DEBUG = True
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = ['8000-jonnydaviso-marketplace-kxhz8xrxb60.ws-eu103.gitpod.io']
+ALLOWED_HOSTS = ['8000-jonnydaviso-marketplace-clsdzlh6337.ws-eu103.gitpod.io']
 # ALLOWED_HOSTS = ['https://market-8c7ce1538eb0.herokuapp.com/']
 
 # Application definition
