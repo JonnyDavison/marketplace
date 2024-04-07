7326fd5202200f93d9c89a2a46f1a94b7b2fffab
JonnyDavison <jonnydavison@icloud.com>
2024-04-07 21:43:56 +0000

    Update settings staticfiles storage

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -155,8 +155,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
