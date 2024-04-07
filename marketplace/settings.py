1390ed3d13a300d1c75b1b850cc547f8dfb2ada6
JonnyDavison <jonnydavison@icloud.com>
2024-04-07 23:28:51 +0000

    Install django_heroku and update requirments

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -156,8 +156,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
+import django_heroku 
+django_heroku.settings(local())
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
